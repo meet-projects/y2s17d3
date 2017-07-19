7a1fdc1b0a748aa0a7e0f391fcdf4991a233f03f
Bassil <bassil@mit.edu>
2017-07-19 12:39:31 +0300

    added lab1.py

---
+++ lab1.py
@@ -0,0 +1,15 @@
+from py_review import *
+
+student_example = Student("sample student")
+
+# print(student_example.get_name())
+
+# student_example.set_name("INSERT NAME HERE")
+
+# print(student_example.get_name())
+
+# print(student_example.go_to_lesson())
+
+# print(student_example.go_to_lesson())
+
+# student_example.focus()
