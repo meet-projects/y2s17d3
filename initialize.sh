5d46b76af12b0838df6872bddacc21f8b07f6f4f
mnowik <nowicki.matthieu@gmail.com>
2017-07-25 01:16:12 +0300

    add old path to bash

--- initialize.sh
+++ initialize.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+old_pwd=$(pwd)
 cd ~/
 if [[ -e y2-venv ]]; then
     source ~/y2-venv/bin/activate
@@ -7,5 +8,6 @@ else
      sudo apt-get install virtualenv
      virtualenv -p $(which python3) y2-venv
      source ~/y2-venv/bin/activate
-     pip install -r requirements.txt
+     pip install -r $old_pwd/requirements.txt
 fi
+cd $old_pwd
